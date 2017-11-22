f41f6e2860eca12556eefe9d1b52eee56176df58
Ahmed Matoussi <ahmedgeek1@gmail.com>
2017-11-22 15:49:39 -0600

    Add files via upload
    Add complete folder with files of the project

---
+++ Analyzing scores app/Assessment8A/Form1.Designer.cs
@@ -0,0 +1,264 @@
+﻿namespace Assessment8A
+{
+    partial class FormScoresAnalysis
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnSaveFileAnalysis = new System.Windows.Forms.Button();
+            this.btnOpenAndAnalyzeFile = new System.Windows.Forms.Button();
+            this.lblThelowestTestScorePrompt = new System.Windows.Forms.Label();
+            this.lblHighestTestScorePompt = new System.Windows.Forms.Label();
+            this.lblAverageTestScorePrompt = new System.Windows.Forms.Label();
+            this.lblTotalNumberOfScoresPrompt = new System.Windows.Forms.Label();
+            this.lblAverageTestScoreOutput = new System.Windows.Forms.Label();
+            this.lblTotalNumberOfScoreOutput = new System.Windows.Forms.Label();
+            this.lblLowestTestScoreOutput = new System.Windows.Forms.Label();
+            this.lblHighestTestScoreOutput = new System.Windows.Forms.Label();
+            this.openFileDialogScores = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialogScores = new System.Windows.Forms.SaveFileDialog();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblWelcome = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnExit
+            // 
+            this.btnExit.BackColor = System.Drawing.Color.Firebrick;
+            this.btnExit.Font = new System.Drawing.Font("Microsoft YaHei UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.ForeColor = System.Drawing.Color.White;
+            this.btnExit.Location = new System.Drawing.Point(484, 467);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(130, 57);
+            this.btnExit.TabIndex = 0;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = false;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.BackColor = System.Drawing.Color.Firebrick;
+            this.btnClear.Font = new System.Drawing.Font("Microsoft YaHei UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClear.ForeColor = System.Drawing.Color.White;
+            this.btnClear.Location = new System.Drawing.Point(219, 467);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(144, 57);
+            this.btnClear.TabIndex = 1;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = false;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnSaveFileAnalysis
+            // 
+            this.btnSaveFileAnalysis.BackColor = System.Drawing.Color.Firebrick;
+            this.btnSaveFileAnalysis.Font = new System.Drawing.Font("Microsoft YaHei UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSaveFileAnalysis.ForeColor = System.Drawing.Color.White;
+            this.btnSaveFileAnalysis.Location = new System.Drawing.Point(312, 385);
+            this.btnSaveFileAnalysis.Name = "btnSaveFileAnalysis";
+            this.btnSaveFileAnalysis.Size = new System.Drawing.Size(211, 57);
+            this.btnSaveFileAnalysis.TabIndex = 2;
+            this.btnSaveFileAnalysis.Text = "Save Analysis";
+            this.btnSaveFileAnalysis.UseVisualStyleBackColor = false;
+            this.btnSaveFileAnalysis.Click += new System.EventHandler(this.btnSaveFileAnalysis_Click);
+            // 
+            // btnOpenAndAnalyzeFile
+            // 
+            this.btnOpenAndAnalyzeFile.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.btnOpenAndAnalyzeFile.Font = new System.Drawing.Font("Microsoft YaHei UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnOpenAndAnalyzeFile.ForeColor = System.Drawing.Color.White;
+            this.btnOpenAndAnalyzeFile.Location = new System.Drawing.Point(56, 182);
+            this.btnOpenAndAnalyzeFile.Name = "btnOpenAndAnalyzeFile";
+            this.btnOpenAndAnalyzeFile.Size = new System.Drawing.Size(215, 55);
+            this.btnOpenAndAnalyzeFile.TabIndex = 3;
+            this.btnOpenAndAnalyzeFile.Text = "Open And Analyze a file";
+            this.btnOpenAndAnalyzeFile.UseVisualStyleBackColor = false;
+            this.btnOpenAndAnalyzeFile.Click += new System.EventHandler(this.btnOpenAndAnalyseFile_Click);
+            // 
+            // lblThelowestTestScorePrompt
+            // 
+            this.lblThelowestTestScorePrompt.Font = new System.Drawing.Font("Microsoft YaHei UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblThelowestTestScorePrompt.Location = new System.Drawing.Point(407, 279);
+            this.lblThelowestTestScorePrompt.Name = "lblThelowestTestScorePrompt";
+            this.lblThelowestTestScorePrompt.Size = new System.Drawing.Size(190, 59);
+            this.lblThelowestTestScorePrompt.TabIndex = 4;
+            this.lblThelowestTestScorePrompt.Text = "The Lowest Test Score";
+            this.lblThelowestTestScorePrompt.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblHighestTestScorePompt
+            // 
+            this.lblHighestTestScorePompt.Font = new System.Drawing.Font("Microsoft YaHei UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHighestTestScorePompt.Location = new System.Drawing.Point(407, 212);
+            this.lblHighestTestScorePompt.Name = "lblHighestTestScorePompt";
+            this.lblHighestTestScorePompt.Size = new System.Drawing.Size(193, 59);
+            this.lblHighestTestScorePompt.TabIndex = 5;
+            this.lblHighestTestScorePompt.Text = "The Highest Test Score";
+            this.lblHighestTestScorePompt.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblHighestTestScorePompt.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // lblAverageTestScorePrompt
+            // 
+            this.lblAverageTestScorePrompt.Font = new System.Drawing.Font("Microsoft YaHei UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAverageTestScorePrompt.Location = new System.Drawing.Point(407, 146);
+            this.lblAverageTestScorePrompt.Name = "lblAverageTestScorePrompt";
+            this.lblAverageTestScorePrompt.Size = new System.Drawing.Size(177, 59);
+            this.lblAverageTestScorePrompt.TabIndex = 6;
+            this.lblAverageTestScorePrompt.Text = "Average Test Score";
+            this.lblAverageTestScorePrompt.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblTotalNumberOfScoresPrompt
+            // 
+            this.lblTotalNumberOfScoresPrompt.Font = new System.Drawing.Font("Microsoft YaHei UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTotalNumberOfScoresPrompt.Location = new System.Drawing.Point(407, 83);
+            this.lblTotalNumberOfScoresPrompt.Name = "lblTotalNumberOfScoresPrompt";
+            this.lblTotalNumberOfScoresPrompt.Size = new System.Drawing.Size(198, 59);
+            this.lblTotalNumberOfScoresPrompt.TabIndex = 7;
+            this.lblTotalNumberOfScoresPrompt.Text = "Total Number Of Scores";
+            this.lblTotalNumberOfScoresPrompt.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblAverageTestScoreOutput
+            // 
+            this.lblAverageTestScoreOutput.BackColor = System.Drawing.Color.White;
+            this.lblAverageTestScoreOutput.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblAverageTestScoreOutput.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.lblAverageTestScoreOutput.Font = new System.Drawing.Font("Microsoft YaHei UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAverageTestScoreOutput.Location = new System.Drawing.Point(630, 167);
+            this.lblAverageTestScoreOutput.Name = "lblAverageTestScoreOutput";
+            this.lblAverageTestScoreOutput.Size = new System.Drawing.Size(174, 28);
+            this.lblAverageTestScoreOutput.TabIndex = 11;
+            this.lblAverageTestScoreOutput.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblTotalNumberOfScoreOutput
+            // 
+            this.lblTotalNumberOfScoreOutput.BackColor = System.Drawing.Color.White;
+            this.lblTotalNumberOfScoreOutput.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblTotalNumberOfScoreOutput.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.lblTotalNumberOfScoreOutput.Font = new System.Drawing.Font("Microsoft YaHei UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTotalNumberOfScoreOutput.Location = new System.Drawing.Point(630, 104);
+            this.lblTotalNumberOfScoreOutput.Name = "lblTotalNumberOfScoreOutput";
+            this.lblTotalNumberOfScoreOutput.Size = new System.Drawing.Size(174, 32);
+            this.lblTotalNumberOfScoreOutput.TabIndex = 12;
+            this.lblTotalNumberOfScoreOutput.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblLowestTestScoreOutput
+            // 
+            this.lblLowestTestScoreOutput.BackColor = System.Drawing.Color.White;
+            this.lblLowestTestScoreOutput.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblLowestTestScoreOutput.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.lblLowestTestScoreOutput.Font = new System.Drawing.Font("Microsoft YaHei UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLowestTestScoreOutput.Location = new System.Drawing.Point(630, 300);
+            this.lblLowestTestScoreOutput.Name = "lblLowestTestScoreOutput";
+            this.lblLowestTestScoreOutput.Size = new System.Drawing.Size(174, 28);
+            this.lblLowestTestScoreOutput.TabIndex = 13;
+            this.lblLowestTestScoreOutput.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblHighestTestScoreOutput
+            // 
+            this.lblHighestTestScoreOutput.BackColor = System.Drawing.Color.White;
+            this.lblHighestTestScoreOutput.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblHighestTestScoreOutput.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.lblHighestTestScoreOutput.Font = new System.Drawing.Font("Microsoft YaHei UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHighestTestScoreOutput.Location = new System.Drawing.Point(630, 233);
+            this.lblHighestTestScoreOutput.Name = "lblHighestTestScoreOutput";
+            this.lblHighestTestScoreOutput.Size = new System.Drawing.Size(174, 27);
+            this.lblHighestTestScoreOutput.TabIndex = 14;
+            this.lblHighestTestScoreOutput.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // openFileDialogScores
+            // 
+            this.openFileDialogScores.FileName = "openFileDialog1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Blue;
+            this.label2.Location = new System.Drawing.Point(708, 529);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(119, 16);
+            this.label2.TabIndex = 20;
+            this.label2.Text = "© Ahmed Matoussi";
+            // 
+            // lblWelcome
+            // 
+            this.lblWelcome.AutoSize = true;
+            this.lblWelcome.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblWelcome.Location = new System.Drawing.Point(184, 18);
+            this.lblWelcome.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblWelcome.Name = "lblWelcome";
+            this.lblWelcome.Size = new System.Drawing.Size(475, 24);
+            this.lblWelcome.TabIndex = 21;
+            this.lblWelcome.Text = "Welcome To Our File Scores Analysis Application";
+            // 
+            // FormScoresAnalysis
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(187)))), ((int)(((byte)(187)))), ((int)(((byte)(204)))));
+            this.ClientSize = new System.Drawing.Size(864, 554);
+            this.Controls.Add(this.lblWelcome);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblHighestTestScoreOutput);
+            this.Controls.Add(this.lblLowestTestScoreOutput);
+            this.Controls.Add(this.lblTotalNumberOfScoreOutput);
+            this.Controls.Add(this.lblAverageTestScoreOutput);
+            this.Controls.Add(this.lblTotalNumberOfScoresPrompt);
+            this.Controls.Add(this.lblAverageTestScorePrompt);
+            this.Controls.Add(this.lblHighestTestScorePompt);
+            this.Controls.Add(this.lblThelowestTestScorePrompt);
+            this.Controls.Add(this.btnOpenAndAnalyzeFile);
+            this.Controls.Add(this.btnSaveFileAnalysis);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnExit);
+            this.Name = "FormScoresAnalysis";
+            this.Text = "Scores Analysis";
+            this.Load += new System.EventHandler(this.FormScoresAnalysis_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnSaveFileAnalysis;
+        private System.Windows.Forms.Button btnOpenAndAnalyzeFile;
+        private System.Windows.Forms.Label lblThelowestTestScorePrompt;
+        private System.Windows.Forms.Label lblHighestTestScorePompt;
+        private System.Windows.Forms.Label lblAverageTestScorePrompt;
+        private System.Windows.Forms.Label lblTotalNumberOfScoresPrompt;
+        private System.Windows.Forms.Label lblAverageTestScoreOutput;
+        private System.Windows.Forms.Label lblTotalNumberOfScoreOutput;
+        private System.Windows.Forms.Label lblLowestTestScoreOutput;
+        private System.Windows.Forms.Label lblHighestTestScoreOutput;
+        private System.Windows.Forms.OpenFileDialog openFileDialogScores;
+        private System.Windows.Forms.SaveFileDialog saveFileDialogScores;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblWelcome;
+    }
+}
+
---
+++ Analyzing scores app/Assessment8A/Form1.cs
@@ -0,0 +1,220 @@
+﻿/*Ahmed Matoussi
+ * email: ahmedgeek1@gmail.com
+ * This application is to read the data from a file and store it in an array of the appropriate size, 
+ * and then analyze the data in the array in a variety of ways and give the user a way to save the analysis
+ */
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Assessment8A
+{
+    public partial class FormScoresAnalysis : Form
+    {
+        public FormScoresAnalysis()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnOpenAndAnalyseFile_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                //declare some variables to use 
+                double averageScore, highScore, lowScore;
+
+                // Declare a StreamReader variable.
+                StreamReader inputFile;
+                // set up initial open directory! 
+                String UN = Environment.UserName; //variable for the username
+                String FilePath = @"C:\Users\" + UN + @"\Documents\"; //concatinating the username with the path
+                openFileDialogScores.InitialDirectory = FilePath;  //this folder is getting showed in the second replicate of the save dialogue window
+                // show only files with .txt extension
+                openFileDialogScores.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+
+                //if the uyser click on open and open a file then
+                if (openFileDialogScores.ShowDialog() == DialogResult.OK) 
+                {
+                    //number of lines in the file
+                    var lineCount = File.ReadAllLines(openFileDialogScores.FileName).Length;
+                    Boolean testRange = false;
+                    if (lineCount <= 0)
+                    {
+                        MessageBox.Show("No records found in this file!");
+                    }
+                    else if (lineCount > 100)
+                    {
+                        MessageBox.Show("The Maximum of lines allowed per file is 100! Please select a file that has less than 100 line");
+                    }
+                    
+                    else {
+                        double[] arrayScores = new double[lineCount];
+                        // openFile.ShowDialog();
+                        inputFile = File.OpenText(openFileDialogScores.FileName);
+                        //attempt to remove blank lines
+                       // var lines = File.ReadAllLines(openFileDialogScores.FileName).Where(arg => !string.IsNullOrWhiteSpace(arg));
+                      //  File.WriteAllLines(openFileDialogScores.FileName, lines);
+                        for (int i = 0; i < lineCount; i++)
+                        {
+                            
+                            
+                            
+                                arrayScores[i] = double.Parse(inputFile.ReadLine());
+                                if (arrayScores[i] > 100 || arrayScores[i] < 0)
+                                {
+                                    testRange = true;
+                                 }
+                            
+                        }
+                        if (testRange == false) { 
+                        //store the results from the methods in variables
+                        averageScore = calculate_Average(arrayScores);
+                        highScore = find_Highest_Score(arrayScores);
+                        lowScore = find_Lowest_Score(arrayScores);
+
+                        //display the results in the labels
+                        lblAverageTestScoreOutput.Text = averageScore.ToString();
+                        lblHighestTestScoreOutput.Text = highScore.ToString();
+                        lblLowestTestScoreOutput.Text = lowScore.ToString();
+                        lblTotalNumberOfScoreOutput.Text = lineCount.ToString();
+
+                        //close the file
+                        inputFile.Close();
+                        }else
+                        {
+                            MessageBox.Show("All scores in the File should be in a range between 0 and 100");
+                        }
+                    }
+                }
+                else //if the user don't open a file then an error message is shown!
+                {
+                    MessageBox.Show("Operation cancelled!");
+                }
+                
+            }
+            catch (Exception ex)
+            {
+                //show the error message
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private double calculate_Average(double[] fileArray)
+        {//method to calculate the average of scores
+            double avg;
+            double sum = 0;
+            for (int i = 0; i < fileArray.Length; i++)
+            {
+                sum = sum + fileArray[i];
+            }
+            avg = (double)sum / fileArray.Length; //calculate the avg
+            return avg; //return the average
+        }
+
+        private double find_Highest_Score(double[] fileArray)
+        {   //a method to find the highest score
+            double HighestScore = 0;
+            for (int i = 0; i < fileArray.Length; i++)
+            {
+                if (fileArray[i] > HighestScore)
+                {
+                    HighestScore = fileArray[i];
+                }
+            }
+            //return the highest score
+            return HighestScore;
+
+        }
+
+        private double find_Lowest_Score(double[] fileArray)
+        {//a method to find the lowest score
+            double LoweststScore = fileArray[0];
+            for (int i = 0; i < fileArray.Length; i++)
+            {
+                if (fileArray[i] < LoweststScore)
+                {
+                    LoweststScore = fileArray[i];
+                }
+            }
+            //return the lowest score
+            return LoweststScore;
+
+        }
+
+        private void btnSaveFileAnalysis_Click(object sender, EventArgs e)
+        {
+          
+            try
+            {
+                    //set the default path for saving the file
+                    saveFileDialogScores.InitialDirectory = @"C:\Users\Public\Documents\";
+                //add default extension
+                // saveFileDialogScores.DefaultExt = ".txt";
+                saveFileDialogScores.Filter = "Text Files (*.txt)|*.txt"/*"|RichText Files (*.rtf)|*.RTF|World Document (*.docx)|*.DOCX"*/;
+
+                    // Declare a StreamWriter variable. 
+                StreamWriter outputFile;
+                    //int LineCount = int.Parse(lblTotalNumberOfScoreOutput.Text);
+                    if(lblAverageTestScoreOutput.Text == "")
+                {
+                    MessageBox.Show("No data to generate!");
+                }
+                    else if (saveFileDialogScores.ShowDialog() == DialogResult.OK) //if the user click on save button
+                    {
+
+                        // Create and append the selected file.
+                        outputFile = File.AppendText(saveFileDialogScores.FileName);
+                        outputFile.WriteLine("The total Number of Scores is = " + lblTotalNumberOfScoreOutput.Text); //write the lines in the file
+                        outputFile.WriteLine("The Average Score = " + lblAverageTestScoreOutput.Text); //write the lines in the file
+                        outputFile.WriteLine("The Lowest score is = " + lblLowestTestScoreOutput.Text); //write the lines in the file
+                        outputFile.WriteLine("The Highest Score is = " + lblHighestTestScoreOutput.Text); //write the lines in the file
+
+                        outputFile.Close(); // Close the file
+
+                        MessageBox.Show("Data generated and File saved successfully!");
+                    }
+                    else //if the user click on cancel button and didn't save any file
+                    {
+                        MessageBox.Show("Operation cancelled!");
+                    }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            //clear all labels
+            lblHighestTestScoreOutput.Text = "";
+            lblLowestTestScoreOutput.Text = "";
+            lblTotalNumberOfScoreOutput.Text = "";
+            lblAverageTestScoreOutput.Text = "";
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            //close this form
+            this.Close();
+        }
+
+        private void FormScoresAnalysis_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
